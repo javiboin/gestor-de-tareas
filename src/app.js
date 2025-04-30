11c83988a2cd81f4e11c0bca09b2f24c969c8170
Javier Alejandro Oyarzo Carcamo <46333397+javiboin@users.noreply.github.com>
2025-04-30 20:41:55 -0300

    Levantar Servidor

--- src/app.js
+++ src/app.js
@@ -1,1 +1,17 @@
-// instalar libreria dot env
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+const host = 'http://localhost';
+const port = process.env.PORT || 3000;
+const url = `${host}:${port}`;
+
+app.use(cors());
+app.use(express.json()); // Permite trabajar con JSON en las solicitudes
+app.use(express.urlencoded({ extended: true })); // Soporta datos codificados en URL
+
+app.listen(port, () =>{
+    console.log(`Servidor escuchando en ${url}`);
+});
