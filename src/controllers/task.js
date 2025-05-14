3f6e9cb2a6917f97095278076554e705abfd1fed
Javier Alejandro Oyarzo Carcamo <46333397+javiboin@users.noreply.github.com>
2025-05-14 20:11:40 -0300

    Update: Get By id from DB

--- src/controllers/task.js
+++ src/controllers/task.js
@@ -24,14 +24,6 @@ const getAllTasks = (req, res) => {
 };
 
 const getTaskById = (req, res) => {
-/*     try {
-        if (data_tasks.length === 0) {
-            return res.status(404).json({ message: 'No hay tareas disponibles' });
-        };
-    } catch (error) {
-        return res.status(500).json({ message: 'Error al obtener la tarea' });
-    }; */
-    // Obtener la tarea por ID de la base de datos
     TaskModel.findById(req.params.id)
         .then(task => {
             if (!task) {
@@ -43,18 +35,6 @@ const getTaskById = (req, res) => {
             console.error('Error al obtener la tarea de la base de datos:', err);
             res.status(500).json({ message: 'Error al obtener la tarea de la base de datos' });
         });
-    
-
-/*     try {
-        const { id } = req.params;
-        const task = data_tasks.find(data => id === data.id);
-        if (!task) {
-            return res.status(404).json({ message: 'Tarea no encontrada' });
-        }
-        res.status(200).json(task);
-    } catch (error) {
-        res.status(500).json({ message: 'Error al obtener la tarea' });
-    }; */
 };
 
 const createTask = (req, res) => {
@@ -84,7 +64,6 @@ const createTask = (req, res) => {
     };
 
     const newTask = { 
-        //id: `${data_tasks.length + 1}`, 
         title: req.body.title,
         content: req.body.content
     };
--- src/models/tasks.js
+++ src/models/tasks.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 
 const taskSchema = new mongoose.Schema({
-    //id: String,
     title: String,
     content: String
 });
